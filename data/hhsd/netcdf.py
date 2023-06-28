dc9ba1f8c9868a0bae98680a88ba5a2c1b9ed611
Your Name <you@example.com>
2023-06-28 10:43:44 +0800

    test np append row

--- data/hhsd/netcdf.py
+++ data/hhsd/netcdf.py
@@ -22,3 +22,16 @@ for f in files:
     print(time)
 #for i in range(120):
 #    print(foot[1][i])
+a = np.arange(3*4).reshape(3,4)
+b = np.arange(3*4).reshape(3,4)
+c=a+b
+print(a)
+print(b)
+print(c)
+c_f=c.flatten(order='C')
+print(c_f)
+d=np.zeros((2,12))
+d[0]=c_f
+d[1]=c_f
+d=np.r_[d,np.expand_dims(c_f,axis=0)]
+print(d)
